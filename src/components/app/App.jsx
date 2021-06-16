a45781c9dc2d82b1c80bac918ea24d348a142837
Jena Boehm <boehmjena@gmail.com>
2021-06-15 22:18:18 -0700

    set up router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { About } from '../about/About';
+import { Contact } from '../contact/Contact';
+import { Home } from '../home/Home';
+import { Portfolio } from '../portfolio/Portfolio';
 
 export const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route 
+          exact
+          path="/"
+          component={Home} />
+        <Route 
+          exact
+          path="/about"
+          component={About} />
+        <Route 
+          exact
+          path="/portfolio"
+          component={Portfolio} />
+        <Route 
+          exact
+          path="/contact"
+          component={Contact} />
+      </Switch>
+    </Router>
   );
 };
