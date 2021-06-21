b6f1ce6425e490c8506e1200364c2802b64093f5
Jena Boehm <boehmjena@gmail.com>
2021-06-21 15:37:35 -0700

    add conditional rendering for headerLeft

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './Header.css';
 
 export const Header = () => {
+  const location = useLocation();
+
   return (
-    <div className={styles.headerContainer}>
-      <Link to="/">Home</Link> &nbsp; | &nbsp;
-      <Link to="/portfolio">Portfolio</Link>&nbsp;  | &nbsp;
-      <Link to="/about">About</Link> &nbsp; | &nbsp;
-      <Link to="/contact">Contact</Link> 
-    </div>
+    <>
+      {location.pathname === '/' 
+        ? (
+          <div className={styles.headerContainer}>
+            <section className={styles.headerRight}>
+              <Link to="/">Home</Link> &nbsp; | &nbsp;
+              <Link to="/portfolio">Portfolio</Link>&nbsp;  | &nbsp;
+              <Link to="/about">About</Link> &nbsp; | &nbsp;
+              <Link to="/contact">Contact</Link> 
+            </section>
+          </div>
+
+        ) : (
+          <div className={styles.headerContainer}>
+            <section className={styles.headerLeft}>
+              <div className={styles.name}>
+                Jena Lux Boehm</div>
+            </section>
+            <section className={styles.headerRight}>
+              <Link to="/">Home</Link> &nbsp; | &nbsp;
+              <Link to="/portfolio">Portfolio</Link>&nbsp;  | &nbsp;
+              <Link to="/about">About</Link> &nbsp; | &nbsp;
+              <Link to="/contact">Contact</Link> 
+            </section>
+          </div>
+        )}
+    </>
   );
 };
