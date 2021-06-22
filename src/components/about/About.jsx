655acc9d0f4cac61298e213b6da09ea2b35a9999
Jena Boehm <boehmjena@gmail.com>
2021-06-21 22:54:47 -0700

    about title and top section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,30 @@
+/* eslint-disable max-len */
 import React from 'react';
 import styles from './About.css';
+import headshot from '../../../public/images/headshot.jpg';
 
 export const About = () => {
   return (
     <div className={styles.aboutContainer}>
-            About
+      <h1 className={styles.titleLeft}>
+            ABOUT
+      </h1>
+      <section className={styles.aboutTop}>
+        <div className={styles.crop}>
+          <img
+            className={styles.headshot} 
+            src={headshot}
+            alt="Jena headshot"
+            height="50%"
+            width="auto"/>
+        </div>
+        <div className={styles.description}>
+          Multifaceted, intuitive, and detail-driven Full Stack Software Engineer.
+        </div>
+      </section>
+      <section className={styles.aboutMain}>
+        
+      </section>
     </div>
   );
 };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import styles from './Header.css';
+import leftBracket from '../../../public/images/curly-brackets-left.png';
+import rightBracket from '../../../public/images/curly-brackets-right.png';
 
 export const Header = () => {
   const location = useLocation();
@@ -10,11 +12,26 @@ export const Header = () => {
       {location.pathname === '/' 
         ? (
           <div className={styles.headerContainer}>
+            <section className={styles.headerLeft}>
+              <div></div>
+            </section>
             <section className={styles.headerRight}>
+              <img 
+                className={styles.brackets}
+                src={leftBracket}
+                alt="left curly bracket"
+                height="25vh"
+              />
               <Link to="/">Home</Link> &nbsp; | &nbsp;
               <Link to="/portfolio">Portfolio</Link>&nbsp;  | &nbsp;
               <Link to="/about">About</Link> &nbsp; | &nbsp;
               <Link to="/contact">Contact</Link> 
+              <img 
+                className={styles.brackets}
+                src={rightBracket}
+                alt="right curly bracket"
+                height="25vh"
+              />
             </section>
           </div>
 
